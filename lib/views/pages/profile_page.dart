6f81fc00b64de12bfffce2aed3e6e197fb271c80
Jeyaraj Justin <jjustnotin@gmail.com>
2026-05-31 18:03:41 +0800

    placeholders - ms1

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -19,8 +19,34 @@ class _ProfilePageState extends State<ProfilePage> {
             Row(children: [
               CircleAvatar(radius: 30.0, backgroundColor: Colors.teal,),
               Padding(padding: EdgeInsets.only(left: 10.0)),
-              Text("Arya", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),)
-            ],)
+              Text("Arya", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
+              Padding(padding: EdgeInsets.only(top: 4.0, left: 120.0)),
+              Text("6 following \n7 followers", style: TextStyle(fontSize: 17.0),)
+            ],),
+            Padding(padding: EdgeInsets.all(10.0)),
+            Container(
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Text(
+                "My Photos",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(2.0)),
+            Container(
+              margin: EdgeInsets.only(left: 5.0, top: 5.0, right: 5.0),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(),
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.blueGrey,
+                    ),
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                ]))
           ],)
       ),
     );
